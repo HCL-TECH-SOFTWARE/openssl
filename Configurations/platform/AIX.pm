df12d7ccc2aa895a3c695aa946f15379f597f2c1
John Kohl <john.kohl@hcl.com>
2023-07-18 16:58:57 -0400

    add support for pure shlibs, not shlibs inside .a files, for AIX

--- Configurations/platform/AIX.pm
+++ Configurations/platform/AIX.pm
@@ -13,12 +13,15 @@ require platform::Unix;
 use configdata;
 
 sub dsoext              { '.so' }
-sub shlibextsimple      { '.a' }
+sub shlibextsimple      { return '.so' if $target{shared_target} eq "aix-svr4";
+			  '.a'}
 
 # In shared mode, the default static library names clashes with the final
 # "simple" full shared library name, so we add '_a' to the basename of the
-# static libraries in that case.
+# static libraries in that case, unless in svr4 mode (using only .so files)
 sub staticname {
+    return platform::Unix->staticname($_[1]) if $target{shared_target} eq "aix-svr4";
+
     # Non-installed libraries are *always* static, and their names remain
     # the same, except for the mandatory extension
     my $in_libname = platform::BASE->staticname($_[1]);
